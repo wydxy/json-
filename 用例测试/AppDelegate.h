c87d8044ef1c0c37f8c2d62b7a9d479f4f7ac323
nianfangge <nianfangge@nianfanggedeMacBook-Pro.local>
2015-07-25 11:58:57 +0800

    Initial Commit

---
+++ 用例测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  用例测试
+//
+//  Created by nianfangge on 15/7/25.
+//  Copyright (c) 2015年 farben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 用例测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  用例测试
+//
+//  Created by nianfangge on 15/7/25.
+//  Copyright (c) 2015年 farben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
